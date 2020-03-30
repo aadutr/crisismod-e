ead9ffac97c2139be2cc9fde872650f597131c2a
myrthesmit <m.m.smit-1@student.tudelft.nl>
2020-03-30 14:24:34 +0200

    Readable code now + unnecessary statements removed

--- infectionModel_v0.1.py
+++ infectionModel_v0.1.py
@@ -1,107 +1,116 @@
-
-
-# disease spread
-from cycler import cycler
+# importing required modules
 import matplotlib.pyplot as plt
 import numpy as np 
 from scipy.integrate import solve_ivp
 import os
 
-# make room for figures for later
+# save the figures in a separate folder
 if not os.path.exists('figures'):
   os.mkdir('figures')
 
 colors = ['#299727ff', '#b6311cff', '#276ba2ff', '#424242ff']
 
 def populationModel(t, n, r, p):
-  HP  = n[0]
-  SP1 = n[1]
-  SP2 = n[2]
-  SP3 = n[3]
-  IP  = n[4]
-  DP  = n[5]
+  """ Define the population model based on the different rates and initial conditions 
 
-  TP = HP + SP1 + SP2 + SP3 + IP
+    Parameters
+    ----------
+    t : Time array
+    n : Fraction of the population that is in a given state (susceptible, asymptomatic, symptomatic, hospitalized, recovered or dead).
+    r : Rates array.
+    p : Defines the capacity of the health care system (currently determines how many IC beds there are for the population).
 
-  r_meeting1      = r[ 0]
-  r_meeting2      = r[ 1]
-  r_meeting3      = r[ 2]
-  r_infection1    = r[ 3]
-  r_infection2    = r[ 4]
-  r_infection3    = r[ 5]
-  r_sym           = r[ 6]
-  r_hos           = r[ 7]
-  r_d1            = r[ 8]
-  r_d2            = r[ 9]
-  r_im1           = r[10]
-  r_im2           = r[11]
-  r_im3           = r[12]
+    Returns
+    ------
+    Array dn containing the combination of rates that describe the system of ODEs 
+    
+    """
+    
+  HP  = n[0] #susceptible people
+  IP1 = n[1] #infected but not symptomatic people
+  IP2 = n[2] #symptomatic patients
+  IP3 = n[3] #hospitalized patients
+  RP  = n[4] #recovered people
+  DP  = n[5] #dead people
+
+  TP = HP + IP1 + IP2 + IP3 + RP #total population
+
+  r_meeting1      = r[ 0] #rate at which susceptible people meet asymptomatic patients
+  r_meeting2      = r[ 1] #rate at which susceptible people meet symptomatic patients
+  r_meeting3      = r[ 2] #rate at which susceptible people meet hospitalized patients
+  r_infection1    = r[ 3] #rate at which asymptomatic patients infect people
+  r_infection2    = r[ 4] #rate at which symptomatic patients infect people
+  r_infection3    = r[ 5] #rate at which hospitalized patients infect people
+  r_sym           = r[ 6] #rate at which asymptomatic patients get symptoms
+  r_hos           = r[ 7] #rate at which symptomatic patients become hospitalized
+  r_d1            = r[ 8] #death rate 1: rate constant for dying due to the disease
+  r_d2            = r[ 9] #death rate 2: relative increae in death rate when IC beds are full 
+  r_im1           = r[10] #recovery rate for asymptomatic patients
+  r_im2           = r[11] #recovery rate for symptomatic patients
+  r_im3           = r[12] #recovery rate for hospitalized patients
   
-  cap_ER          = p[ 0]
+  cap_IC          = p[ 0] #number of IC beds
 
-  R_inf1 = r_meeting1 * r_infection1 * (HP / TP) * SP1
-  R_inf2 = r_meeting2 * r_infection2 * (HP / TP) * SP2
-  R_inf3 = r_meeting3 * r_infection3 * (HP / TP) * SP3
-  r_d = r_d1 + logistic(SP3, cap_ER) * r_d2
+  R_inf1 = r_meeting1 * r_infection1 * (HP / TP) * IP1 #infection rate 1: chance they susceptible people meet asymptomatic patients ánd that they are infected
+  R_inf2 = r_meeting2 * r_infection2 * (HP / TP) * IP2 #infection rate 2: people are infected by symptomatic patients
+  R_inf3 = r_meeting3 * r_infection3 * (HP / TP) * IP3 #infection rate 3: people are infected by hospitalized patients
+  r_d = r_d1 + logistic(IP3, cap_IC) * r_d2
   
-  dn = np.empty(len(n))
+  dn = np.empty(len(n)) #create an empty array to define the ODEs
 
   dn[0] = - R_inf1 - R_inf2 - R_inf3
-  dn[1] = + R_inf1 + R_inf2 + R_inf3 - r_sym * SP1               - r_im1 * SP1
-  dn[2] =                            + r_sym * SP1 - r_hos * SP2               - r_im2 * SP2  
-  dn[3] =                                          + r_hos * SP2                             - r_im3 * SP3 - r_d * SP3
-  dn[4] =                                                        + r_im1 * SP1 + r_im2 * SP2 + r_im3 * SP3
-  dn[5] =                                                                                                  + r_d * SP3
+  dn[1] = + R_inf1 + R_inf2 + R_inf3 - r_sym * IP1               - r_im1 * IP1
+  dn[2] =                            + r_sym * IP1 - r_hos * IP2               - r_im2 * IP2  
+  dn[3] =                                          + r_hos * IP2                             - r_im3 * IP3 - r_d * IP3
+  dn[4] =                                                        + r_im1 * IP1 + r_im2 * IP2 + r_im3 * IP3
+  dn[5] =                                                                                                  + r_d * IP3
   
   return dn
 
-# adapted from https://math.stackexchange.com/a/2869151
-# def jacPopulationModel(t, n, r):
-#   eps = np.finfo(np.float).eps
-#   J = np.zeros([len(n), len(n)])
 
-#   for i in range(len(n)):
-#     n1 = n.copy()
-#     n2 = n.copy()
+def logistic(IP3, cap_IC):
+    """ Creates a sigmoid shape that accomodates for the increase in death rate when the IC beds are full. You need this because otherwise the ODEs can not be solved. 
 
-#     n1[i] += eps
-#     n2[i] -= eps
+    Parameters
+    ----------
+    IP3 : Float
+    Fraction of the population that is hospitalized.
+    cap_IC : Float
+    Maximum capacity of the IC.
 
-#     f1 = populationModel(t, n1, r)
-#     f2 = populationModel(t, n2, r)
+    Returns
+    -------
+    FLOAT
+    Sigmoid function.
 
-#     J[:, i] = (f1 - f2) / (2 * eps)
+    """
+    return np.exp(IP3 - cap_IC) / (np.exp(IP3 - cap_IC) + 1)
 
-#   return J
 
-def logistic(SP3, cap_ER):
-  return np.exp(SP3 - cap_ER) / (np.exp(SP3 - cap_ER) + 1)
-
+#Declaration of the input variables
+    
 # r_meeting1  r_meeting2  r_meeting3  r_infection1  r_infection2  r_infection3  r_sym  r_hos   r_d1  r_d2  r_im1  r_im2  r_im3
 r = [     10,          5,          1,         0.01,          0.3,          0.1,  0.05,  0.02,  0.01, 0.04,  0.05,  0.08,   0.1]
 
-p = [1250 / 8.4e6] # 1250 beds for 8.4 mln people
+p = [1250 / 8.4e6] # Determines how many IC beds are available. Now 1250 beds for 8.4 mln people
+n0 = [0.9999975, 0.0000025, 0, 0, 0, 0]  #initial conditions: fraction of the population that is in a certain state. 
 
+#creating the time array
 tstart = 0
 tend   = 365
 tspan = (tstart, tend)
-# print(tspan.shape())
-n0 = [0.9999975, 0.0000025, 0, 0, 0, 0]
-
-sol = solve_ivp(populationModel, tspan, n0, args=[r, p], dense_output=True)
-
-print(sol.message)
-
 t = np.linspace(tstart, tend, tend * 100)
 
+#solving the system of ODEs 
+sol = solve_ivp(populationModel, tspan, n0, args=[r, p], dense_output=True)
+print(sol.message)
 y = sol.sol(t)
 
+#plotting the solution
 fig1, ax1 = plt.subplots()
 
-# ax1.set_prop_cycle(cycler(color=colors))
-
 ax1.plot(t, y.transpose(), linewidth=3)
-ax1.legend(['HP', 'SP1', 'SP2', 'SP3', 'IP', 'DP'])
+ax1.legend(['HP', 'IP1', 'IP2', 'IP3', 'RP', 'DP'])
 ax1.set_title('Population disease model')
 ax1.set_ylabel('Fraction of people')
 ax1.set_xlabel('Time (days)')
