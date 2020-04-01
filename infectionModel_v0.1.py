99b4cadfa83c60ebf75ed8a3caaf9fdf2f4a9269
Anouk Dutrée <a.a.dutree@student.tudelft.nl>
2020-04-01 19:04:42 +0200

    Added code for calculating and plotting the amount of new cases per group per day

--- infectionModel_v0.1.py
+++ infectionModel_v0.1.py
@@ -39,6 +39,27 @@ ax1.set_xlabel('Time (days)')
 
 fig1.savefig('figures/Population disease model_2.png')
 
+#calculating the per day increase of cases
+# The idea is if you have an array [1,2,3] and you want to know the increments between
+# each element you can create an array [0, 1, 2] and subtract that from the original matrix
+# and then you will get the differences between each consecutive element.
+y_shift = np.zeros((len(y)-1,tend))     #empty matrix
+endpoint = tend*100
+y_perday = y[:,0:endpoint:100]          #Taking out the data points for each exact day
+y_shift[:,1:] = y_perday[1:,0:tend-1:1] #Putting it in the new matrix with position shifted by 1
+delta_y = (y_perday[1:,:] - y_shift)    #calculating the difference in cases for each category between consecutive days
+t_days = np.linspace(tstart, tend, tend)#having a correct time array for plotting purposes
+
+#plotting the amount of new cases per day per group
+fig2, ax2 = plt.subplots()
+ax2.plot(t_days,delta_y.transpose(), linewidth = 3)
+ax2.legend([ 'Asymptomatic', 'Symptomatic', 'Hospitalized', 'ICU', 'Recovered', 'Dead'], loc = "right")
+ax2.set_title('New cases/day of disease')
+ax2.set_ylabel('New cases as fraction of people')
+ax2.set_xlabel('Time (days)')
+
+fig2.savefig('figures/newcaserate.png')
+
 plt.show()
 
 
